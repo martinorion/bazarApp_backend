7a78604cc608ae1d6381bd1485c9298f1c45f77d
martinorion <martinhox03@gmail.com>
2023-04-18 20:28:01 +0200

    online

--- src/main/java/com/example/backend/Category/CategoryController.java
+++ src/main/java/com/example/backend/Category/CategoryController.java
@@ -1,12 +1,17 @@
 package com.example.backend.Category;
 
+import com.example.backend.Image.Image;
+import com.example.backend.Image.ImageUtility;
+import com.example.backend.ImageOfCategory.ImageOfCategory;
 import com.example.backend.ImageOfCategory.ImageOfCategoryRepository;
+import com.example.backend.Products.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -27,7 +32,21 @@ public class CategoryController {
 
    @GetMapping("/getallcategories")
     public List<Category> getAllCategories(){
-         return (List<Category>) categoryRepository.findAll();
+        List<Category> categoryList = (List<Category>) categoryRepository.findAll();
+
+       ArrayList<Category> categories = new ArrayList<>();
+
+       for(Category category : categoryList){
+           category.setImageOfCategory(
+                   ImageOfCategory.builder()
+                           .id(category.getImageOfCategory().getId())
+                           .type(category.getImageOfCategory().getType())
+                           .image(ImageUtility.decompressImage(category.getImageOfCategory().getImage())).build());
+
+           categories.add(category);
+       }
+
+       return categories;
     }
 
 }
